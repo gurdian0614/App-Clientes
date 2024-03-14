42c298a7b27bd58ce1a5a50302289ca69f1d08f3
gurdian0614 <ivan.gurdian@outlook.com>
2024-03-13 18:26:45 -0600

    Quitando archivos innecesarios, agregando empleado  main

--- App Clientes/App.xaml.cs
+++ App Clientes/App.xaml.cs
@@ -1,4 +1,6 @@
-﻿namespace App_Clientes
+﻿using App_Clientes.Views;
+
+namespace App_Clientes
 {
     public partial class App : Application
     {
@@ -6,7 +8,7 @@
         {
             InitializeComponent();
 
-            MainPage = new AppShell();
+            MainPage = new ClienteMainPage();
         }
     }
 }
---
+++ App Clientes/Views/ClienteMainPage.xaml.cs
@@ -0,0 +1,9 @@
+namespace App_Clientes.Views;
+
+public partial class ClienteMainPage : ContentPage
+{
+	public ClienteMainPage()
+	{
+		InitializeComponent();
+	}
+}
