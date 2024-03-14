b4d96b0d5a46bfbbfbb5d30fde3cf0b19de7d25f
gurdian0614 <ivan.gurdian@outlook.com>
2024-03-13 18:26:58 -0600

    Creando modelo

---
+++ App Clientes/Models/Cliente.cs
@@ -0,0 +1,20 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App_Clientes.Models
+{
+    public class Cliente
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Direccion { get; set; }
+        public string Telefono { get; set; }
+        public string Email { get; set; }
+        public double LimiteCredito { get; set; }
+    }
+}
